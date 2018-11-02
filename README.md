d65f39a9401d36af5183b5b0625a8dee6004d3ef
nozaryo <nozaryo0730@gmail.com>
2018-11-02 22:56:26 +0900

    settted option , chenged readme

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # FESS
 F.T.E. Rocket Simulating System
+## Features
+F.E.S.S.
+├─Detail mood
+│  ├─Trajectory
+│  └─Open Parachute
+└─Scatter mood
+    ├─Trajectory
+    │  ├─Single output
+    │  └─Multiple output
+    └─Open Parachute
+       ├─Single output
+       └─Multiple output
 # Authors
 nozaryo
--- run.py
+++ run.py
@@ -4,5 +4,36 @@ Edited by Ryoya Nozaki
 
 reference
 https://github.com/Shun0228/RocketSimulation
+https://qiita.com/taashi/items/400871fb13df476f42d2
 
 """
+import os
+import sys
+import math
+from argparse import ArgumentParser
+import numpy as np
+import scipy as sp
+import numpy.linalg as nplin
+from scipy.integrate import odeint
+
+import calculation as cal
+import quaternion as qt
+import environment as env
+import summary as summ
+
+
+def get_option():
+    args = ArgumentParser(prog='F.T.E. Rocket Simulating System')
+    args.add_argument('-J','--json',default="",type=str,
+                           help='json file name ')
+    args.add_argument('-M','--mood',
+                            type=str, choices=['d','s'], nargs=1, metavar='mood',
+                            help="Select mood d:detail s:scatter")
+    args.add_argument('-P','--parachute',
+                            type=int, choices=[0,1], nargs=1, metavar='open_mood',
+                            help="Select parachute mood 0: Trajectory,  1: Open one stage parachute")
+    return args.parse_args()
+
+if __name__ == '__main__':
+    args = get_option()
+    print(args)
